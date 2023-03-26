40cafd4cc8a6804edd2c6f0badf3c544b2529152
Ethan P <eth-p+git@hidden.email>
2023-03-25 22:14:10 -0700

    build: Target es2020 for bigint support

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -35,7 +35,7 @@ const context = await esbuild.context({
 		'.md': 'text',
 	},
 	format: 'cjs',
-	target: 'es2018',
+	target: 'es2020',
 	logLevel: 'info',
 	sourcemap: prod ? false : 'inline',
 	treeShaking: true,
